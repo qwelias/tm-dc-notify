ca0dfe40474b310867cbb8ad7a88508fc5ca91ec
Ilia Baryshnikov <qwelias@gmail.com>
2021-04-07 14:06:50 +0200

    chore: wording

--- src/global.ts
+++ src/global.ts
@@ -1,5 +1,3 @@
-// gotta fucking fix TS myself every time cuse they live in a fucking wonderland
-
 interface JSON {
     parse(text: string | Buffer, reviver?: (this: any, key: string, value: any) => any): any;
 }
